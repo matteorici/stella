bbd18700c5f6a400d2a1ab04d8631c461973c0b8
matteorici <matteorici@proton.me>
2026-07-22 18:23:36 +0300

    Create sample_data.go

---
+++ sample_data.go
@@ -0,0 +1,79 @@
+package main
+
+var Prefixes = map[string][]string{
+
+	"Elven": {
+		"Ael",
+		"Ela",
+		"Syl",
+		"Luna",
+		"Fae",
+	},
+
+	"Dwarven": {
+		"Kar",
+		"Dur",
+		"Mor",
+		"Thor",
+	},
+
+	"Nordic": {
+		"Skal",
+		"Frost",
+		"Bjorn",
+		"Nord",
+	},
+
+	"Desert": {
+		"Al",
+		"Qar",
+		"Zah",
+		"Mir",
+	},
+
+	"Imperial": {
+		"Val",
+		"Port",
+		"Cast",
+		"Nova",
+	},
+}
+
+var Suffixes = map[string][]string{
+
+	"Elven": {
+		"oria",
+		"wyn",
+		"moor",
+		"reth",
+		"anor",
+	},
+
+	"Dwarven": {
+		"hold",
+		"forge",
+		"heim",
+		"rock",
+	},
+
+	"Nordic": {
+		"fjord",
+		"vik",
+		"gard",
+		"heim",
+	},
+
+	"Desert": {
+		"abad",
+		"zir",
+		"ara",
+		"mar",
+	},
+
+	"Imperial": {
+		"polis",
+		"ium",
+		"ford",
+		"gate",
+	},
+}
